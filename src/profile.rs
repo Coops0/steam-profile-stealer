a684601684ace04b0d9c235f8783da361aa7c1a4
Coops0 <cooperhanessian@gmail.com>
2023-05-17 18:08:16 -0400

    unit tests

--- src/profile.rs
+++ src/profile.rs
@@ -47,7 +47,7 @@ pub async fn get_self_profile(wrapper: &mut WebsocketWrapper) -> Result<Profile>
         .context("couldn't parse profile (server error)")
 }
 
-pub async fn parse_profile(wrapper: &mut WebsocketWrapper, url: &str) -> anyhow::Result<Profile> {
+pub async fn parse_profile(wrapper: &mut WebsocketWrapper, url: &str) -> Result<Profile> {
     wrapper.log(format!("Sending request to your profile {url}...")).await;
     let resp = Client::default().get(url)
         .header(header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
@@ -65,7 +65,10 @@ pub async fn parse_profile(wrapper: &mut WebsocketWrapper, url: &str) -> anyhow:
         // for some fucking reason there are 2 elements,
         // one w/ the name and the other is " "
         .filter(|e| !e.is_empty())
-        .collect::<String>();
+        .collect::<Vec<&str>>()
+        .first()
+        .context("no first element")?
+        .to_string();
 
     let icon_url = document.select(&Selector::parse(".playerAvatarAutoSizeInner > img").unwrap())
         .filter_map(|e| e.value().attr("src"))
@@ -79,4 +82,37 @@ pub async fn parse_profile(wrapper: &mut WebsocketWrapper, url: &str) -> anyhow:
             url: url.to_string(),
         }
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::websocket::WebsocketWrapper;
+    use anyhow::Result;
+    use crate::profile::{get_self_profile, parse_profile};
+    const AUTH_COOKIE: &str = "76561198286609782%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MTFEMF8yMjMxODEzRl9DRkNDRCIsICJzdWIiOiAiNzY1NjExOTgyODY2MDk3ODIiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY4NDQzODUyNywgIm5iZiI6IDE2NzU3MTE2MjMsICJpYXQiOiAxNjg0MzUxNjIzLCAianRpIjogIjBEMUVfMjI4REE3MDhfQzAxQ0EiLCAib2F0IjogMTY3ODMwMTkwMywgInJ0X2V4cCI6IDE2OTYzOTQwOTksICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICI3MS4xOTEuODQuMjgiLCAiaXBfY29uZmlybWVyIjogIjcxLjE5MS44NC4yOCIgfQ.95-Oc8Q02HRQKEv2z82CV7M2KZ-BHwGU4pxQzb17_qKffoxM67WTb0vqTgwgv6s8F9PQXDwvpNsE9AL0JkM8BA";
+
+    #[tokio::test]
+    async fn get_self_profile_test() -> Result<()> {
+        let mut wrapper = WebsocketWrapper::new(None);
+        wrapper.cookie = AUTH_COOKIE.to_owned();
+
+        let profile = get_self_profile(&mut wrapper).await?;
+        assert_eq!(&profile.icon_url, "https://avatars.akamai.steamstatic.com/244734af0daa603a8ae9f33f5dcb3a7c14c4d126_full.jpg");
+        assert_eq!(&profile.name, "your friend");
+        assert_eq!(&profile.url, "https://steamcommunity.com/id/coops_");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn parse_profile_test() -> Result<()> {
+        let mut wrapper = WebsocketWrapper::new(None);
+        let profile = parse_profile(&mut wrapper, "https://steamcommunity.com/id/gabelogannewell").await?;
+
+        assert_eq!(&profile.icon_url, "https://avatars.akamai.steamstatic.com/c5d56249ee5d28a07db4ac9f7f60af961fab5426_full.jpg");
+        assert_eq!(&profile.name, "Rabscuttle");
+        assert_eq!(&profile.url, "https://steamcommunity.com/id/gabelogannewell");
+
+        Ok(())
+    }
 }
--- src/stealer.rs
+++ src/stealer.rs
@@ -48,11 +48,9 @@ pub async fn headless_steam(wrapper: &mut WebsocketWrapper, name: &str, base64_i
     page.set_cookie(cookie).await?;
 
     wrapper.log("Set cookie, navigating to edit info page...").await;
-
     page.goto(format!("{}/edit/info", wrapper.profile_url)).await?;
 
     wrapper.log("Navigated to edit info page, loading...").await;
-
     // wait to load
     let _ = page.wait_for_navigation_response().await?;
 
@@ -104,7 +102,6 @@ pub async fn headless_steam(wrapper: &mut WebsocketWrapper, name: &str, base64_i
     wrapper.log("Finished updating image.").await;
 
     let _ = page.wait_for_navigation().await?.content().await?;
-
     wrapper.log("Page finished navigation, closing browser.").await;
 
     browser.close().await?;
@@ -115,18 +112,25 @@ pub async fn headless_steam(wrapper: &mut WebsocketWrapper, name: &str, base64_i
 #[cfg(test)]
 mod tests {
     use anyhow::Result;
+    use crate::profile::get_self_profile;
     use crate::stealer::{headless_steam, image_to_base64};
     use crate::websocket::WebsocketWrapper;
+    const AUTH_COOKIE: &str = "76561198286609782%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MTFEMF8yMjMxODEzRl9DRkNDRCIsICJzdWIiOiAiNzY1NjExOTgyODY2MDk3ODIiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY4NDQzODUyNywgIm5iZiI6IDE2NzU3MTE2MjMsICJpYXQiOiAxNjg0MzUxNjIzLCAianRpIjogIjBEMUVfMjI4REE3MDhfQzAxQ0EiLCAib2F0IjogMTY3ODMwMTkwMywgInJ0X2V4cCI6IDE2OTYzOTQwOTksICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICI3MS4xOTEuODQuMjgiLCAiaXBfY29uZmlybWVyIjogIjcxLjE5MS44NC4yOCIgfQ.95-Oc8Q02HRQKEv2z82CV7M2KZ-BHwGU4pxQzb17_qKffoxM67WTb0vqTgwgv6s8F9PQXDwvpNsE9AL0JkM8BA";
 
     #[tokio::test]
     async fn test_image_shit() -> Result<()> {
         let mut wrapper = WebsocketWrapper::new(None);
-        wrapper.cookie = "76561198286609782%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MTFEMF8yMjMxODEzRl9DRkNDRCIsICJzdWIiOiAiNzY1NjExOTgyODY2MDk3ODIiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY4NDQzODUyNywgIm5iZiI6IDE2NzU3MTE2MjMsICJpYXQiOiAxNjg0MzUxNjIzLCAianRpIjogIjBEMUVfMjI4REE3MDhfQzAxQ0EiLCAib2F0IjogMTY3ODMwMTkwMywgInJ0X2V4cCI6IDE2OTYzOTQwOTksICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICI3MS4xOTEuODQuMjgiLCAiaXBfY29uZmlybWVyIjogIjcxLjE5MS44NC4yOCIgfQ.95-Oc8Q02HRQKEv2z82CV7M2KZ-BHwGU4pxQzb17_qKffoxM67WTb0vqTgwgv6s8F9PQXDwvpNsE9AL0JkM8BA"
-            .to_owned();
+        wrapper.cookie = AUTH_COOKIE.to_owned();
         wrapper.profile_url = "https://steamcommunity.com/id/coops_".to_owned();
 
         let img = image_to_base64(&mut wrapper, "https://avatars.cloudflare.steamstatic.com/3c783af9215d49b3daa150d95444489aede2f855_full.jpg").await?;
 
-        headless_steam(&mut wrapper, "your friend", &img).await
+        headless_steam(&mut wrapper, "your friend", &img).await?;
+
+        let profile = get_self_profile(&mut wrapper).await?;
+        assert_eq!(&profile.name, "your friend");
+        assert_eq!(profile.url, wrapper.profile_url);
+
+        Ok(())
     }
 }
